18222c74a68dfee93050848e38e9d2d7bba2b3cc
Jose Alonso <mail@josealonso.dev>
2025-04-16 21:47:11 -0600

    Initial Products API Rutes. CRUD

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,39 @@
-import { Controller } from '@nestjs/common';
-import { ProductsService } from './products.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly productsService: ProductsService) {}
+  constructor() {}
+
+  @Post()
+  createProduct() {
+    return 'Crea un producto';
+  }
+
+  @Get()
+  findAllProducts() {
+    return 'Lista todos los productos';
+  }
+
+  @Get(':id')
+  findOneProduct(@Param('id') id: string) {
+    return 'Esta funcion regresa el producto ' + id;
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+    return 'Esta funcion elimina el producto ' + id;
+  }
+
+  @Patch(':id')
+  updateProduct(@Param('id') id: string, @Body() body: any) {
+    return 'Esta funcion actualiza el producto ' + id;
+  }
 }

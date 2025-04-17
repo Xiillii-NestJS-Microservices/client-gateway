0e46cea37cff826d487c5fca39c999881f920db8
Jose Alonso <mail@josealonso.dev>
2025-04-16 23:38:47 -0600

    Fix custom exception filter error

--- src/common/exceptions/rpc-custom-exception.filter.ts
+++ src/common/exceptions/rpc-custom-exception.filter.ts
@@ -1,20 +1,32 @@
 import { Catch, ArgumentsHost, ExceptionFilter } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 
+interface CustomRpcError {
+  message: string | string[];
+  status?: number | string; // Make status optional and allow string
+}
+
 @Catch(RpcException)
 export class RpcCustomExceptionFilter implements ExceptionFilter<RpcException> {
   catch(exception: RpcException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
 
-    const rpcError = exception.getError();
+    const rpcError = exception.getError() as CustomRpcError; // Type assertion
+
+    const message = Array.isArray(rpcError.message)
+      ? rpcError.message
+      : [rpcError.message];
+    let status = 400; // Default status
 
     if (
       typeof rpcError === 'object' &&
       'status' in rpcError &&
       'message' in rpcError
     ) {
-      const status = isNaN(+rpcError.status) ? 400 : +rpcError.status;
+      if (rpcError.status !== undefined) {
+        status = isNaN(+rpcError.status) ? 400 : +rpcError.status;
+      }
 
       return response.status(status).json(rpcError);
     }

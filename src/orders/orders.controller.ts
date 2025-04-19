416bce3caf48b2ea186537f229262c933ec3c4c5
Jose Alonso <mail@josealonso.dev>
2025-04-18 22:14:26 -0600

    Consume Orders Microservice

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Post, Body, Param, Inject } from '@nestjs/common';
+
+import { ORDER_SERVICE } from 'src/config';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError } from 'rxjs';
+import { CreateOrderDto } from './dto';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(
+    @Inject(ORDER_SERVICE) private readonly ordersClient: ClientProxy,
+  ) {}
+
+  @Post()
+  create(@Body() createOrderDto: CreateOrderDto) {
+    return this.ordersClient.send('createOrder', createOrderDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.ordersClient.send('findAllOrders', {});
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.ordersClient.send('findOneOrder', { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
+  }
+
+  @Post('changeStatus')
+  changeStatus(@Body() createOrderDto: CreateOrderDto) {
+    return this.ordersClient.send('changeOrderStatus', createOrderDto);
+  }
+}

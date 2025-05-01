c1a251845096f3350470ecd8d76bdc9218086e6a
Jose Alonso <mail@josealonso.dev>
2025-04-30 21:44:01 -0600

    Tcp to NATS the Orders communication

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -10,7 +10,7 @@ import {
   Patch,
 } from '@nestjs/common';
 
-import { ORDER_SERVICE } from 'src/config';
+import { NATS_SERVICE } from 'src/config';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
 import { CreateOrderDto, OrderPaginationDto, StatusDto } from './dto';
@@ -18,49 +18,47 @@ import { PaginationDto } from 'src/common';
 
 @Controller('orders')
 export class OrdersController {
-  // constructor(
-  //   @Inject(ORDER_SERVICE) private readonly ordersClient: ClientProxy,
-  // ) {}
-  // @Post()
-  // create(@Body() createOrderDto: CreateOrderDto) {
-  //   return this.ordersClient.send('createOrder', createOrderDto);
-  // }
-  // @Get(':status')
-  // findAllByStatus(
-  //   @Param() statusDto: StatusDto,
-  //   @Query() paginationDto: PaginationDto,
-  // ) {
-  //   return this.ordersClient.send('findAllOrders', {
-  //     ...paginationDto,
-  //     status: statusDto.status,
-  //   });
-  // }
-  // @Get()
-  // findAll(@Query() paginationDto: OrderPaginationDto) {
-  //   return this.ordersClient.send('findAllOrders', paginationDto);
-  // }
-  // @Get('id/:id')
-  // findOne(@Param('id', ParseUUIDPipe) id: string) {
-  //   return this.ordersClient.send('findOneOrder', { id }).pipe(
-  //     catchError((err) => {
-  //       throw new RpcException(err);
-  //     }),
-  //   );
-  // }
-  // @Patch(':id')
-  // changeStatus(
-  //   @Param('id', ParseUUIDPipe) id: string,
-  //   @Body() statusDto: StatusDto,
-  // ) {
-  //   return this.ordersClient
-  //     .send('changeOrderStatus', {
-  //       id: id,
-  //       ...statusDto,
-  //     })
-  //     .pipe(
-  //       catchError((err) => {
-  //         throw new RpcException(err);
-  //       }),
-  //     );
-  // }
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
+  @Post()
+  create(@Body() createOrderDto: CreateOrderDto) {
+    return this.client.send('createOrder', createOrderDto);
+  }
+  @Get(':status')
+  findAllByStatus(
+    @Param() statusDto: StatusDto,
+    @Query() paginationDto: PaginationDto,
+  ) {
+    return this.client.send('findAllOrders', {
+      ...paginationDto,
+      status: statusDto.status,
+    });
+  }
+  @Get()
+  findAll(@Query() paginationDto: OrderPaginationDto) {
+    return this.client.send('findAllOrders', paginationDto);
+  }
+  @Get('id/:id')
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.client.send('findOneOrder', { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
+  }
+  @Patch(':id')
+  changeStatus(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() statusDto: StatusDto,
+  ) {
+    return this.client
+      .send('changeOrderStatus', {
+        id: id,
+        ...statusDto,
+      })
+      .pipe(
+        catchError((err) => {
+          throw new RpcException(err);
+        }),
+      );
+  }
 }

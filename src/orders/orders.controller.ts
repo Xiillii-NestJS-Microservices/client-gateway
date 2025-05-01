3bba7d8b4bc39a40d8a5e5108d567868bcce5f69
Jose Alonso <mail@josealonso.dev>
2025-04-30 21:00:55 -0600

    Setup NATS for Products microservice, and disable, temporaly, Orders Micrservice

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -18,54 +18,49 @@ import { PaginationDto } from 'src/common';
 
 @Controller('orders')
 export class OrdersController {
-  constructor(
-    @Inject(ORDER_SERVICE) private readonly ordersClient: ClientProxy,
-  ) {}
-
-  @Post()
-  create(@Body() createOrderDto: CreateOrderDto) {
-    return this.ordersClient.send('createOrder', createOrderDto);
-  }
-
-  @Get(':status')
-  findAllByStatus(
-    @Param() statusDto: StatusDto,
-    @Query() paginationDto: PaginationDto,
-  ) {
-    return this.ordersClient.send('findAllOrders', {
-      ...paginationDto,
-      status: statusDto.status,
-    });
-  }
-
-  @Get()
-  findAll(@Query() paginationDto: OrderPaginationDto) {
-    return this.ordersClient.send('findAllOrders', paginationDto);
-  }
-
-  @Get('id/:id')
-  findOne(@Param('id', ParseUUIDPipe) id: string) {
-    return this.ordersClient.send('findOneOrder', { id }).pipe(
-      catchError((err) => {
-        throw new RpcException(err);
-      }),
-    );
-  }
-
-  @Patch(':id')
-  changeStatus(
-    @Param('id', ParseUUIDPipe) id: string,
-    @Body() statusDto: StatusDto,
-  ) {
-    return this.ordersClient
-      .send('changeOrderStatus', {
-        id: id,
-        ...statusDto,
-      })
-      .pipe(
-        catchError((err) => {
-          throw new RpcException(err);
-        }),
-      );
-  }
+  // constructor(
+  //   @Inject(ORDER_SERVICE) private readonly ordersClient: ClientProxy,
+  // ) {}
+  // @Post()
+  // create(@Body() createOrderDto: CreateOrderDto) {
+  //   return this.ordersClient.send('createOrder', createOrderDto);
+  // }
+  // @Get(':status')
+  // findAllByStatus(
+  //   @Param() statusDto: StatusDto,
+  //   @Query() paginationDto: PaginationDto,
+  // ) {
+  //   return this.ordersClient.send('findAllOrders', {
+  //     ...paginationDto,
+  //     status: statusDto.status,
+  //   });
+  // }
+  // @Get()
+  // findAll(@Query() paginationDto: OrderPaginationDto) {
+  //   return this.ordersClient.send('findAllOrders', paginationDto);
+  // }
+  // @Get('id/:id')
+  // findOne(@Param('id', ParseUUIDPipe) id: string) {
+  //   return this.ordersClient.send('findOneOrder', { id }).pipe(
+  //     catchError((err) => {
+  //       throw new RpcException(err);
+  //     }),
+  //   );
+  // }
+  // @Patch(':id')
+  // changeStatus(
+  //   @Param('id', ParseUUIDPipe) id: string,
+  //   @Body() statusDto: StatusDto,
+  // ) {
+  //   return this.ordersClient
+  //     .send('changeOrderStatus', {
+  //       id: id,
+  //       ...statusDto,
+  //     })
+  //     .pipe(
+  //       catchError((err) => {
+  //         throw new RpcException(err);
+  //       }),
+  //     );
+  // }
 }
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -13,35 +13,27 @@ import {
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
 import { PaginationDto } from 'src/common';
-import { PRODUCT_SERVICE } from 'src/config';
+import { NATS_SERVICE } from 'src/config';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
-  constructor(
-    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy,
-  ) {}
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post()
   createProduct(@Body() createProductDto: CreateProductDto) {
-    return this.productsClient.send(
-      { cmd: 'create_product' },
-      createProductDto,
-    );
+    return this.client.send({ cmd: 'create_product' }, createProductDto);
   }
 
   @Get()
   findAllProducts(@Query() paginationDto: PaginationDto) {
-    return this.productsClient.send(
-      { cmd: 'find_all_products' },
-      paginationDto,
-    );
+    return this.client.send({ cmd: 'find_all_products' }, paginationDto);
   }
 
   @Get(':id')
   findOneProduct(@Param('id', ParseIntPipe) id: number) {
-    return this.productsClient.send({ cmd: 'find_one_product' }, { id }).pipe(
+    return this.client.send({ cmd: 'find_one_product' }, { id }).pipe(
       catchError((err) => {
         throw new RpcException(err);
       }),
@@ -50,7 +42,7 @@ export class ProductsController {
 
   @Delete(':id')
   deleteProduct(@Param('id', ParseIntPipe) id: number) {
-    return this.productsClient.send({ cmd: 'delete_product' }, { id }).pipe(
+    return this.client.send({ cmd: 'delete_product' }, { id }).pipe(
       catchError((err) => {
         throw new RpcException(err);
       }),
@@ -62,7 +54,7 @@ export class ProductsController {
     @Param('id', ParseIntPipe) id: number,
     @Body() updateProductDto: UpdateProductDto,
   ) {
-    return this.productsClient
+    return this.client
       .send({ cmd: 'update_product' }, { id, ...updateProductDto })
       .pipe(
         catchError((err) => {

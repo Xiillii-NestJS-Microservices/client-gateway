f6e06b5fa703a40df474290076bd1d0c278108ab
José Alonso <mail@josealonso.dev>
2025-05-05 23:33:17 -0600

    Refactor authentication module: remove AuthService and related tests, implement NATS messaging in AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Controller, Get, Inject, Post } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { NATS_SERVICE } from 'src/config';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
+
+  @Post('register')
+  registerUser() {
+    return this.client.send('auth.register.user', {});
+  }
+
+  @Post('login')
+  loginUser() {
+    return this.client.send('auth.login.user', {});
+  }
+
+  @Get('verify')
+  verifyToken() {
+    return this.client.send('auth.verify.token', {});
+  }
 }

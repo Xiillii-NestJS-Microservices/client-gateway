ea5750ae951898ffc09b86c07747fe6232ce5ae0
José Alonso <mail@josealonso.dev>
2025-05-06 22:27:00 -0600

    Refactor AuthGuard to use ClientProxy for token verification and user retrieval

--- src/auth/guard/auth.guard.ts
+++ src/auth/guard/auth.guard.ts
@@ -1,14 +1,20 @@
 import {
   CanActivate,
   ExecutionContext,
+  Inject,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 import { Request } from 'express';
+import { firstValueFrom } from 'rxjs';
+import { NATS_SERVICE } from 'src/config';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
+
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
@@ -18,12 +24,11 @@ export class AuthGuard implements CanActivate {
     }
 
     try {
-      request['user'] = {
-        id: 1,
-        name: '233',
-        email: '323',
-      };
-      request['token'] = token;
+      const { user, token: newToken } = await firstValueFrom(
+        this.client.send('auth.verify.token', token),
+      );
+      request['user'] = user;
+      request['token'] = newToken;
     } catch {
       throw new UnauthorizedException();
     }
